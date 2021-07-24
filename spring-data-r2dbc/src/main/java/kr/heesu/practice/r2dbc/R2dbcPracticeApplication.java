8b43a0f5f5cf012bdbb47ad93a19fb6b6519a613
Hee Su Lee <hesue615@gmail.com>
2021-07-24 13:28:37 +0900

    Initial commit

---
+++ spring-data-r2dbc/src/main/java/kr/heesu/practice/r2dbc/R2dbcPracticeApplication.java
@@ -0,0 +1,12 @@
+package kr.heesu.practice.r2dbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class R2dbcPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(R2dbcPracticeApplication.class, args);
+    }
+}
